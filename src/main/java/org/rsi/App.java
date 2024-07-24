d4f213275a424d6d9afdd117a65ffa1fc6a8515f
bdaf <kamilkorenkiewiczpw@gmail.com>
2024-07-24 18:08:19 +0200

    moved path from hardcoded to argument

--- src/main/java/org/rsi/App.java
+++ src/main/java/org/rsi/App.java
@@ -5,7 +5,7 @@ import java.io.File;
 public class App
 {
     public static void main( String[] args ) {
-        File folder = new File("/Users/kamil/tmp");
+        File folder = new File(args[0]);
 
         String[] files = folder.list();
 
