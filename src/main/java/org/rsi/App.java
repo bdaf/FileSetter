4d6f8e9e633b9a130b1f12592955b81cbf7badc3
bdaf <kamilkorenkiewiczpw@gmail.com>
2024-07-24 18:07:12 +0200

    see how tto list all files in directory

--- src/main/java/org/rsi/App.java
+++ src/main/java/org/rsi/App.java
@@ -1,13 +1,17 @@
 package org.rsi;
 
-/**
- * Hello world!
- *
- */
-public class App 
+import java.io.File;
+
+public class App
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main( String[] args ) {
+        File folder = new File("/Users/kamil/tmp");
+
+        String[] files = folder.list();
+
+        for (String file : files)
+        {
+            System.out.println(file);
+        }
     }
 }
